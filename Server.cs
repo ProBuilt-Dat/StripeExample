406f89b73c00a03d6f5581ab180337e7bdabbd3f
Dat <Dat@HQ-DAT>
2024-03-13 16:39:34 -0500

    new code

--- Server.cs
+++ Server.cs
@@ -19,7 +19,7 @@ namespace StripeExample
     public static void Main(string[] args)
     {
       WebHost.CreateDefaultBuilder(args)
-        .UseUrls("http://0.0.0.0:4242")
+        .UseUrls("http://www-stripe.probuiltinfra.com:4242")
         .UseWebRoot("public")
         .UseStartup<Startup>()
         .Build()
